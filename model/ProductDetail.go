4c4c63e79ed9512f2b788f13552ad0a977e13a67
Raina <rainy5sep@gmail.com>
2021-08-15 20:18:06 +0900

    Calldb- GetProductDetailSelection 추가

--- model/ProductDetail.go
+++ model/ProductDetail.go
@@ -1,15 +1,15 @@
 package model
 
 type ProductDetail struct {
-	ProductId        int
-	ProductCode      string
-	ProductName      string
-	ProductUrl       string
-	Description      string
-	Detail           Detail
-	DeliveryInfo     DeliveryInfo
-	ProductPrice     int
-	ProductOption    []string
-	SellerInfo       SellerInfo
-	EcoCertification EcoCertification
+	ProductId     int
+	ProductCode   string
+	ProductName   string
+	ProductUrl    string
+	Description   string
+	Detail        Detail
+	DeliveryInfo  DeliveryInfo
+	ProductPrice  int
+	ProductOption []string
+	Seller        SellerInfo
+	EcoCert       EcoCertification
 }
