6a17648ba295b3f41eb9da4433e9ed64174ee5db
Raina <rainy5sep@gmail.com>
2021-10-11 15:34:14 +0900

    omitempty

--- model/ProductDetail.go
+++ model/ProductDetail.go
@@ -2,17 +2,17 @@ package model
 
 type ProductDetail struct {
 	ProductId        int
-	ProductCode      string
+	ProductCode      string `json:"ProductCode,omitempty"`
 	ProductName      string
 	ProductUrl       string
-	Description      string
-	IsUsed           int
-	Detail           string
-	DeliveryTime     string
-	ShippingFee      string
+	Description      string `json:"Description,omitempty"`
+	IsUsed           int    `json:"IsUsed,omitempty"`
+	Detail           string `json:"Detail,omitempty"`
+	DeliveryTime     string `json:"DeliveryTime,omitempty"`
+	ShippingFee      string `json:"ShippingFee,omitempty"`
 	ProductPrice     int
-	ProductOption    string
-	Seller           SellerInfo
+	ProductOption    string     `json:"ProductOption,omitempty"`
+	Seller           SellerInfo `json:"Seller,omitempty"`
 	Thumbnail        string
-	Ecocertification string
+	Ecocertification string `json:"Ecocertification,omitempty"`
 }
